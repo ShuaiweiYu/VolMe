b933b8282674f6051bc4f63b7c82921b5759f7ac
Shuaiwei YU <shuaiwei.yu@tum.de>
2024-08-07 14:34:14 +0800

    bug fix

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,7 +1,13 @@
 import emailjs from '@emailjs/nodejs'
 
 export const sendCodeEmail = (params) => {
+    return emailjs.send(process.env.EMAIL_SERVICEID, "send_code", params, {
+        publicKey: process.env.EMAIL_PUBLIC,
+        privateKey: process.env.EMAIL_PRIVATE});
 };
 
 export const sendEmail = (params) => {
+    return emailjs.send(process.env.EMAIL_SERVICEID, "generic_template", params, {
+        publicKey: process.env.EMAIL_PUBLIC,
+        privateKey: process.env.EMAIL_PRIVATE});
 };
